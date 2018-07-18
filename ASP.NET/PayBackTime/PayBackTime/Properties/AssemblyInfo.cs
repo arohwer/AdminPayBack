03437823401fcbc02ccb91cb119dd2699b17cb0f
Maddy Richard <MRichard@student.neumont.edu>
2018-07-18 10:30:35 -0600

    Added folders for scripts, css, json. Added CalendarController

--- ASP.NET/PayBackTime/PayBackTime/Properties/AssemblyInfo.cs
+++ ASP.NET/PayBackTime/PayBackTime/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8770827a-0b8c-47e6-b205-73a953647f70")]
+[assembly: Guid("80139490-b913-4d0e-b899-5cf56e828dbc")]
 
 // Version information for an assembly consists of the following four values:
 //
