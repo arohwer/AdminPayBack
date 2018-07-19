a96ad6273fbb8721080498c8fc29444c07b7c52c
Unknown <Ndelgado@student.neumont.edu>
2018-07-19 14:27:06 -0600

    Setup initial (no database)

---
+++ pay_back_time/ModelLibrary/services/PaybackService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary.services
+{
+    //This is where we will make our queries to the database
+    class PaybackService
+    {
+
+    }
+}
---
+++ pay_back_time/pay_back_time/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pay_back_time.Startup))]
+namespace pay_back_time
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
